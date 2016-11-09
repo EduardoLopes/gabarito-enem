aec775c122afc0277dbe57b884dc225582d08bb3
Eduardo Lopes <EduardoLopesDS@gmail.com>
2016-11-09 15:27:50 -0300

    Add first day gabaritos

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Button from 'react-bootstrap/lib/Button';
 import Col from 'react-bootstrap/lib/Col';
-import data from './../gabaritos/2016/caderno-2-amarelo.json';
+import dia1Azul from './../gabaritos/2016/dia1-azul.json';
+import dia1Amarelo from './../gabaritos/2016/dia1-amarelo.json';
+import dia1branco from './../gabaritos/2016/dia1-branco.json';
+import dia1Rosa from './../gabaritos/2016/dia1-rosa.json';
+
+const gabaritosDayOne = [];
+
+gabaritosDayOne['azul'] = dia1Azul;
+gabaritosDayOne['amarelo'] = dia1Amarelo;
+gabaritosDayOne['branco'] = dia1branco;
+gabaritosDayOne['rosa'] = dia1Rosa;
 
 class Letter extends React.Component {
-
 
   constructor(props) {
 
@@ -37,20 +46,18 @@ class Question extends React.Component {
   constructor(props) {
 
     super(props);
-    this.state = {correct: this.props.correct, id: this.props.id};
     this.handleClick = this.handleClick.bind(this);
+    this.state = {};
 
   }
 
   handleClick(letter){
 
-    this.setState({choosed: letter});
+    this.props.setLetter(this.props.id, letter);
 
   }
 
   render(){
-
-
 
     const letters = ['A', 'B', 'C', 'D', 'E'];
     const listItems = letters.map(function(letter){
@@ -58,16 +65,14 @@ class Question extends React.Component {
       var className;
       var bsStyle;
 
-      if(typeof(this.state.choosed) != 'undefined'){
+      if(typeof(this.props.choosed) != 'undefined'  && this.props.choosed !== null){
 
-        if(this.state.choosed == this.state.correct && this.state.choosed == letter || this.state.choosed != this.state.correct && this.state.correct == letter){
+        if(this.props.choosed == this.props.correct && this.props.choosed == letter || this.props.choosed != this.props.correct && this.props.correct == letter){
           className = 'correct';
-          //bsStyle = 'success';
         }
 
-        if(this.state.choosed != this.state.correct && this.state.choosed == letter){
+        if(this.props.choosed != this.props.correct && this.props.choosed == letter){
           className = 'wrong';
-          //bsStyle = 'danger';
         }
 
       }
@@ -78,13 +83,13 @@ class Question extends React.Component {
 
     var bsStyle;
 
-    if(typeof(this.state.choosed) != 'undefined'){
+    if(typeof(this.props.choosed) !== 'undefined' && this.props.choosed !== null){
 
-      if(this.state.choosed == this.state.correct){
+      if(this.props.choosed == this.props.correct){
         bsStyle = 'success';
       }
 
-      if(this.state.choosed != this.state.correct){
+      if(this.props.choosed != this.props.correct){
         bsStyle = 'danger';
       }
 
@@ -94,7 +99,7 @@ class Question extends React.Component {
       <Col xs={12} md={4} lg={3} >
         <ButtonGroup justified>
           <div className="btn-group" role="group">
-            <Button bsStyle={bsStyle} bsSize="large" active>{this.state.id}</Button>
+            <Button bsStyle={bsStyle} bsSize="large" active>{this.props.id}</Button>
           </div>
           {listItems}
         </ButtonGroup>
@@ -114,18 +119,49 @@ class App extends React.Component {
 
     super(props);
 
+    this.currentQuestions = {};
+
     this.state = {
-      dayOne : 'amarelo',
+      dayOne : null,
       dayTwo : null,
       dayOneColors: ['azul', 'amarelo', 'branco', 'rosa'],
       dayTwoColors: ['amarelo', 'cinza', 'azul', 'rosa'],
     }
 
+    this.handleSetLetter = this.handleSetLetter.bind(this);
+
+  }
+
+  handleSetLetter(id, letter){
+
+    this.currentQuestions[id] = {
+        correct: this.currentQuestions[id].correct,
+        choosed : letter
+    }
+
+    this.setState({
+      currentQuestions: this.currentQuestions
+    });
+
   }
 
   colorClickHandle(e){
 
-    this.setState({dayOne: e});
+    let data = gabaritosDayOne[e];
+
+    for (let d in data){
+
+      this.currentQuestions[d] = {
+        correct: data[d],
+        choosed : null
+      }
+
+    }
+
+    this.setState({
+      dayOne: e,
+      currentQuestions: this.currentQuestions
+    });
 
   }
 
@@ -133,8 +169,10 @@ class App extends React.Component {
 
     const questionItems = [];
 
-    for (var d in data){
-      questionItems.push(<Question id={d} key={d.toString()} correct={data[d]}/>)
+    for (let d in this.state.currentQuestions){
+
+      questionItems.push(<Question setLetter={this.handleSetLetter} id={d} key={d.toString()} correct={this.state.currentQuestions[d].correct} choosed={this.state.currentQuestions[d].choosed}/>)
+
     }
 
     const listItems = this.state.dayOneColors.map(function(color){
